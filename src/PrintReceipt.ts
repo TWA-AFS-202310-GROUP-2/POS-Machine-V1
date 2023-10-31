1f123d3d8d61f61175d7d0bd27aca0828406f794
Hong Wu <hong.wu@rea-group.com>
2023-10-31 20:31:44 +0800

    feat: create the workflow of printReceipt

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,6 +1,38 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { loadAllItems, loadPromotions } from './Dependencies'
+
+interface Quantity {
+    value: number;
+    quantifier: string
+}
+
+interface ReceiptItem {
+    name: string;
+    quantity: Quantity;
+    unitPrice: number;
+    subtotal: number;
+    discountedPrice: number
+}
+
+interface Tag {
+    barcode: string;
+    quantity: number;
+}
 
 export function printReceipt(tags: string[]): string {
+  const parsedTags: Tag[] = parseTags(tags)
+  const receiptItems: ReceiptItem[] = generateReceiptItems(parsedTags)
+  return renderReceipt(receiptItems)
+}
+
+function parseTags(tags: string[]): Tag[] {
+  return []
+}
+
+function generateReceiptItems(parsedTags: Tag[]): ReceiptItem[] {
+  return []
+}
+
+function renderReceipt(receiptItems: ReceiptItem[]): string {
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
